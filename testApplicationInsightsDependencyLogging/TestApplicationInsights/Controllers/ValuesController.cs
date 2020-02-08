4d0ed4b41bd0c3143bf0920452114e467ab8adbb
Venkata Bharath Reddy Karnati <vekarnat@microsoft.com>
2020-02-07 17:35:48 -0800

    Test Application Insights Dependencies

---
+++ testApplicationInsightsDependencyLogging/TestApplicationInsights/Controllers/ValuesController.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TestApplicationInsights.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        // GET api/values
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<string>>> GetAsync()
+        {
+            HttpClientHandler handler = new HttpClientHandler
+            {
+                // Set max connections per server to int.MaxValue (default is 2 with .NET Framework)
+                MaxConnectionsPerServer = int.MaxValue,
+            };
+            HttpClient httpClient = new HttpClient(handler);
+            HttpRequestMessage request = new HttpRequestMessage(HttpMethod.Get, "https://localhost:44323/api/apitest");
+            try
+            {
+                HttpResponseMessage httpResponseMessage = await httpClient.SendAsync(request, HttpCompletionOption.ResponseContentRead, CancellationToken.None);
+            }
+            catch (TaskCanceledException e) when (e.InnerException is IOException ioe)
+            {
+                Console.WriteLine("Exception TaskCancelled");
+                throw;
+            }
+
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/values/5
+        [HttpGet("{id}")]
+        public ActionResult<string> Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/values
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/values/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/values/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
